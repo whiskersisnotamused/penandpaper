d92f0b937f62c2eaa3de1577adde02b6f08cb837
Whiskers <whiskers@ubuntu.(none)>
2013-10-18 17:31:42 -0700

    New project!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Penandpaper::Application.config.secret_token = '163364a9827c5e3305cd9fdcfd3615454b450678630fa7c85d2b450ddbaa03c9755c86e5be00818551326c3b68abef4daabfc389b43865ee8601398ef0f85300'
